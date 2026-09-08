dcc72dbaaeb1a84ed55f7943ada2cafe601834e7
Your Name <mikiyuya@ip-10-95-2-149.ap-northeast-1.compute.internal>
2026-09-08 12:16:36 +0900

    4commit

--- cyberdefender/src/cyberdefender/AnswerRecord.java
+++ cyberdefender/src/cyberdefender/AnswerRecord.java
@@ -1,17 +1,11 @@
 package cyberdefender;
 import java.time.LocalDateTime;
 public class AnswerRecord {
-    // 生徒ID
     private int studentId;
-    // 問題ID
     private int questionId;
-    // 選択した答え
     private int selectedAnswer;
-    // 正解かどうか
     private boolean correct;
-    // 得点
     private int score;
-    // 解答日時
     private LocalDateTime date;
     // 解答履歴を作成
     public AnswerRecord(int studentId,int questionId,int selectedAnswer,boolean correct,int score) {
